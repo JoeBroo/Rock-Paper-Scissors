b26e4f107fbfdb21b613e1aaf6d779f7c4707e1c
JoeBroo <jdoe40630@gmail.com>
2023-05-21 22:05:13 -0400

    set choice on button click

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const scissors = 3;
 let playerScore = 0;
 let computerScore = 0;
 
+let playerChoice = '';
+
 let counter = 0;
+
+//Scoreboard to display player and computer scores
+const scoreBoard = document.querySelector('.scores');
+const para = document.createElement('p');
 
 //Button for testing: Increments number of points when button is clicked.
 
@@ -20,44 +26,43 @@ function addPoints() {
 }
 
 
-
-// if playerScore > computerScore return "You win the game"
-// if playerScore > computerScore return "You lose the game"
-
 // Plays round when user clicks Start Game button
 const startGame = document.querySelector('.start');
 startGame.addEventListener('click', roundOne);
 
-// Alerts user with the choice they made
+//Sets player's choice to Rock
 const btnRock = document.querySelector('.rock');
 btnRock.addEventListener('click', () => {
-    alert('You chose "Rock". ')
+    const playerChoice = 'Rock';
+    roundOne(playerChoice);
 })
 
+//Sets player's choice to Paper
 const btnPaper = document.querySelector('.paper');
 btnPaper.addEventListener('click', () => {
-    alert('You chose "Paper". ')
+    const  playerChoice = 'Paper';
+    roundOne(playerChoice)
 })
 
+//Sets player's choice to Scissors
 const btnScissors = document.querySelector('.scissors'); 
 btnScissors.addEventListener('click', () => {
-    alert('You chose "Scissors". ');
+    const playerChoice = 'Scissors';
+    roundOne(playerChoice)
 })
 
-// Plays one round of Rock Paper Scissors.
+//Plays a round of the game
 function roundOne(playerChoice, computerChoice) {
-    playerChoice = prompt('Choose "Rock", "Paper", or "Scissors". ');
+
+
     computerChoice = Math.floor(Math.random() * 3) + 1;
 
-    // Display player and computer choices.
     console.log(playerChoice);
     console.log(computerChoice);
 
-    //playerChoice is Rock and computerChoice is Rock.
     if (playerChoice == 1 || playerChoice == "Rock" && computerChoice == 1) {
         console.log("It's a draw! ");
 
-        //playerChoice is Paper and computerChoice is Rock.
     } else if (playerChoice == 2 || playerChoice == "Paper" && computerChoice == 1) {
         console.log('You chose "Paper" and Computer chose "Rock". You win! ');
 
@@ -65,7 +70,6 @@ function roundOne(playerChoice, computerChoice) {
 
         console.log(`Your current score is ${playerScore} ` );
 
-        //playerChoice is Scissors and computerChoice is Rock.
     } else if (playerChoice == 3 || playerChoice == "Scissors" && computerChoice == 1) {
         console.log('You chose "Scissors" and Computer chose "Rock". You lose. ');
 
@@ -73,7 +77,6 @@ function roundOne(playerChoice, computerChoice) {
 
         console.log(`Computer Score is ${computerScore} `);
 
-        //playerChoice is Rock and computerChoice is Paper. 
     } else if (playerChoice == 1 || playerChoice == "Rock" && computerChoice == 2) {
         console.log('You chose "Rock" and Computer chose "Paper". You lose. ');
 
@@ -81,11 +84,9 @@ function roundOne(playerChoice, computerChoice) {
 
         console.log(`Computer score is ${computerScore} `);
 
-        //playerChoice is Paper and computerChoice is Paper.
     } else if (playerChoice == 2 || playerChoice == "Paper" || 2 && computerChoice == 2) {
         console.log("It's a draw!");
 
-        //playerChoice is Scissors and computerChoice is Paper.
     } else if (playerChoice == 3 || playerChoice == "Scissors" && computerChoice == 2) {
         console.log('You chose "Scissors" and Computer chose "Paper". You win! ');
 
@@ -93,7 +94,6 @@ function roundOne(playerChoice, computerChoice) {
 
         console.log(`Your current score is ${playerScore} `);
 
-        //playerChoice is Rock and computerChoice is Scissors.
     } else if (playerChoice == 1 || playerChoice == "Rock" && computerChoice == 3) {
         console.log('You chose "Rock" and Computer chose "Scissors". You win! ');
 
@@ -101,7 +101,6 @@ function roundOne(playerChoice, computerChoice) {
 
         console.log(`Your current score is ${playerScore} `);
 
-        //playerChoice is Paper and computerChoice Scissors
     } else if (playerChoice == 2 || playerChoice == "Paper" && computerChoice == 3) {
         console.log('You chose "Paper" and Computer chose "Scissors". You lose. ');
         
@@ -109,12 +108,19 @@ function roundOne(playerChoice, computerChoice) {
 
         console.log(`Computer score is ${computerScore} `);
         
-        //playerChoice is Scissors and computerChoice is Scissors
     } else if (playerChoice = 3 || playerChoice == "Scissors" && computerChoice == 3) {
         console.log("It's a draw! ");
 
-        //playerChoice is different from Rock, Paper, or Scissors.
-    } else {
-        console.log('Please choose "Rock", "Paper", or "Scissors". ');
+    // } else {
+    //     console.log('Please choose "Rock", "Paper", or "Scissors". ');
+    }
+
+    if (computerScore === 5) {
+        console.log('Game over. Computer wins. ');
+
+    }
+
+    if (playerScore === 5) {
+        console.log('Game over. You win!');
     }
 }
